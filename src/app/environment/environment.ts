51f1efa2d755a7f982263796104c145a3466127f
azhagusundar24 <azhagusundar24@gmail.com>
2026-07-23 16:34:59 +0530

    Remove environment file from repository #1

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // Get a free key from https://aistudio.google.com/apikey
+  geminiApiKey: 'AQ.Ab8RN6Jao0k7-5D2gpXnEvrA-3tn24EC5P3IM5baPvL4wbfGCQ'
+}; 
